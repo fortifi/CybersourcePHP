8fd0c443e07cf5c425e5ce7b955696db82e82f21
Sam Waters <sam.waters@justdevelop.it>
2015-08-26 15:30:37 +0100

    Enums

---
+++ src/CybersourcePHP/Enums/CardType.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by sam
+ * At: 20/05/2014 10:33
+ */
+namespace CybersourcePHP\Enums;
+
+class CardType
+{
+  const AUTO = null;
+  const Visa = "001";
+  const MasterCard = "002";
+  const AmEx = "003";
+  const Discover = "004";
+  const DinersClub = "005";
+  const CarteBlanche = "006";
+  const JCB = "007";
+  const EnRoute = "014";
+  const JAL = "021";
+  const MaestroUK = "024";
+  const Delta = "031";
+  const VisaElectron = "033";
+  const Dankort = "034";
+  const Laser = "035";
+  const CarteBleue = "036";
+  const CartaSi = "037";
+  const EncodedAccountNumber = "039";
+  const MaestroInternational = "040";
+  const GEMoneyUK = "043";
+  const Style = "045";
+} 
---
+++ src/CybersourcePHP/Enums/ProductCodes.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by sam
+ * At: 27/05/2014 11:21
+ */
+namespace CybersourcePHP\Enums;
+
+class ProductCodes
+{
+  const _default = "default";
+  const Adult = "adult_content";
+  const Coupon = "coupon";
+  const Electronic_Goods = "electronic_good";
+  const Electronic_Software = "electronic_software";
+  const Gift_Certificate = "gift_certificate";
+  const Handling_Only = "handling_only";
+  const Service = "service";
+  const Shipping_Handling = "shipping_and_handling";
+  const Shipping_Only = "shipping_only";
+  const Subscription = "subscription";
+}
