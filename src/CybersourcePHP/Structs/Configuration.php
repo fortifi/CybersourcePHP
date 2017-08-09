c2c744e2b6ec70755bc08e404300337b3ef0cd2e
James Eagle <Jleagle@users.noreply.github.com>
2017-08-09 14:08:16 +0100

    Update Configuration.php (#2)

--- src/CybersourcePHP/Structs/Configuration.php
+++ src/CybersourcePHP/Structs/Configuration.php
@@ -12,6 +12,7 @@ class Configuration
   public $orgID;
   //Probably don't need to change these
   public $wsdl = "https://ics2ws.ic3.com/commerce/1.x/transactionProcessor/CyberSourceTransaction_1.99.wsdl";
+  public $wsdlTest = "https://ics2wstest.ic3.com/commerce/1.x/transactionProcessor/CyberSourceTransaction_1.99.wsdl";
   public $wsseNS = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd";
   public $typeNS = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText";
 
